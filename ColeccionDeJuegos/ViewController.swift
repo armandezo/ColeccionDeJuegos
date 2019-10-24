ea91d814f87846cac2225d9f7d283495e59a42db
Jorge Fernández Oré <jorgearmando_JAF@outlook.com>
2019-10-24 17:29:34 -0500

    Initial Commit

---
+++ ColeccionDeJuegos/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ColeccionDeJuegos
+//
+//  Created by Jorge Fernández Oré on 10/24/19.
+//  Copyright © 2019 Tecsup. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
